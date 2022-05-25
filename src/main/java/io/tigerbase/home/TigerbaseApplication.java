0aeb1a7d867054374c6f15327a1479719380ed53
easttigerbase <neweasttiger7@gmail.com>
2022-05-25 23:54:29 +0900

    first commit

---
+++ src/main/java/io/tigerbase/home/TigerbaseApplication.java
@@ -0,0 +1,13 @@
+package io.tigerbase.home;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TigerbaseApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TigerbaseApplication.class, args);
+    }
+
+}
---
+++ src/test/java/io/tigerbase/home/TigerbaseApplicationTests.java
@@ -0,0 +1,13 @@
+package io.tigerbase.home;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TigerbaseApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
